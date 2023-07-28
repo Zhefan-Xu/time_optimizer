52a304ee84be6e882dbe5183d144d92e4d0e7735
Zhefan-Xu <zhefanx@andrew.cmu.edu>
2023-07-28 19:30:28 -0400

    update

--- include/time_optimizer/timeOptimizer.cpp
+++ include/time_optimizer/timeOptimizer.cpp
@@ -18,19 +18,42 @@ namespace timeOptimizer{
 	void timeOptimizer::loadTrajectory(const std::vector<Eigen::Vector3d>& posData, 
 							const std::vector<Eigen::Vector3d>& velData,
 							const std::vector<Eigen::Vector3d>& accData,
-							const std::vector<Eigen::Vector3d>& timeData){
+							double dt){
 		this->posData_ = posData;
 		this->velData_ = velData;
 		this->accData_ = accData;
-		this->timeData_ = timeData;
+		this->dt_ = dt;
+	}
+
+	void timeOptimizer::loadTimeInterval(const std::vector<std::pair<double, double>>& timeInterval){
+		this->timeInterval_ = timeInterval;
+	}
+
+	void timeOptimizer::divideData(){
+
 	}
 	
 	void timeOptimizer::optimize(){
+		this->divideData();
+
+
 		// create mosek environment
 		MSKrescodee r;
 		MSKenv_t env = NULL;
 		r = MSK_makeenv(&env, NULL);
-		int i = 0;
+		
+		// create task
+		MSKtask_t task = NULL;
+		if (r == MSK_RES_OK){
+			r = MSK_maketask(env, 0, 0, &task);
+
+			if (r == MSK_RES_OK){
+				MSK_linkfunctotaskstream(task, MSK_STREAM_LOG, NULL, printstr);
+
+
+			}
+
+		}
 
 	}
 }
--- include/time_optimizer/timeOptimizer.h
+++ include/time_optimizer/timeOptimizer.h
@@ -11,15 +11,21 @@
 #include <Eigen/Eigen>
 #include <mosek.h>
 
+static void MSKAPI printstr(void *handle,
+                            const char str[]){
+  	printf("%s", str);
+} 
+
 namespace timeOptimizer{
 	class timeOptimizer{
 	private:
 		double vmax_;
 		double amax_;
+		double dt_;
 		std::vector<Eigen::Vector3d> posData_;
 		std::vector<Eigen::Vector3d> velData_;
 		std::vector<Eigen::Vector3d> accData_;
-		std::vector<Eigen::Vector3d> timeData_;
+		std::vector<std::pair<double, double>> timeInterval_;
 
 
 	public:
@@ -28,7 +34,9 @@ namespace timeOptimizer{
 		void loadTrajectory(const std::vector<Eigen::Vector3d>& posData, 
 							const std::vector<Eigen::Vector3d>& velData,
 							const std::vector<Eigen::Vector3d>& accData,
-							const std::vector<Eigen::Vector3d>& timeData);
+							double dt);
+		void loadTimeInterval(const std::vector<std::pair<double, double>>& timeInterval);
+		void divideData();
 		void optimize();
 	};
 }
