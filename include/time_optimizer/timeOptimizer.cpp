d464c7138d2537ee7cdd2b2f0fb7163eae8581ae
Zhefan-Xu <zhefanx@andrew.cmu.edu>
2023-07-28 16:23:11 -0400

    add mosek

--- include/time_optimizer/timeOptimizer.cpp
+++ include/time_optimizer/timeOptimizer.cpp
@@ -7,7 +7,30 @@
 #include <time_optimizer/timeOptimizer.h>
 
 namespace timeOptimizer{
-	timeOptimizer::timeOptimizer(){
+	timeOptimizer::timeOptimizer(){}
+	
+
+	void timeOptimizer::setLimits(double vmax, double amax){
+		this->vmax_ = vmax;
+		this->amax_ = amax;
+	}
+
+	void timeOptimizer::loadTrajectory(const std::vector<Eigen::Vector3d>& posData, 
+							const std::vector<Eigen::Vector3d>& velData,
+							const std::vector<Eigen::Vector3d>& accData,
+							const std::vector<Eigen::Vector3d>& timeData){
+		this->posData_ = posData;
+		this->velData_ = velData;
+		this->accData_ = accData;
+		this->timeData_ = timeData;
+	}
+	
+	void timeOptimizer::optimize(){
+		// create mosek environment
+		MSKrescodee r;
+		MSKenv_t env = NULL;
+		r = MSK_makeenv(&env, NULL);
+		int i = 0;
 
 	}
 }
--- include/time_optimizer/timeOptimizer.h
+++ include/time_optimizer/timeOptimizer.h
@@ -9,13 +9,27 @@
 #define TIME_OPTIMIZER_H
 
 #include <Eigen/Eigen>
+#include <mosek.h>
 
 namespace timeOptimizer{
 	class timeOptimizer{
 	private:
-		
+		double vmax_;
+		double amax_;
+		std::vector<Eigen::Vector3d> posData_;
+		std::vector<Eigen::Vector3d> velData_;
+		std::vector<Eigen::Vector3d> accData_;
+		std::vector<Eigen::Vector3d> timeData_;
+
+
 	public:
 		timeOptimizer();
+		void setLimits(double vmax, double amax);
+		void loadTrajectory(const std::vector<Eigen::Vector3d>& posData, 
+							const std::vector<Eigen::Vector3d>& velData,
+							const std::vector<Eigen::Vector3d>& accData,
+							const std::vector<Eigen::Vector3d>& timeData);
+		void optimize();
 	};
 }
 
